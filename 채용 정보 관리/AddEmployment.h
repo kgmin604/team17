41ff5fd0e8dcde93c7eb7f480d357508fb436632
OK-Bi <81093270+OK-Bi@users.noreply.github.com>
2023-05-25 15:14:20 +0900

    3.1. 채용 정보 등록

---
+++ 채용 정보 관리/AddEmployment.h
@@ -0,0 +1,20 @@
+
+#ifndef AddEmployment_h
+#define AddEmployment_h
+
+#include "Member.h"
+#include "CoporateMember.h"
+
+//AddEmployment
+//채용 정보 등록
+
+
+class AddEmployment
+{
+public:
+    void addNewEmployment(Member* curMem, string part,int NumberOfPeople, string applicationDeadline);
+    
+};
+
+
+#endif /* AddEmployment_hpp */
