a89eda8c6698f134b6d5543f7eaa4e98fdf76dcc
xoxyunn <104340764+xoxyunn@users.noreply.github.com>
2023-05-25 22:29:55 +0900

    Update DeleteAccountUI.h

--- 최종코드_주석X/DeleteAccountUI.h
+++ 최종코드_주석X/DeleteAccountUI.h
@@ -3,10 +3,18 @@
 
 #include "DeleteAccount.h"
 
+/*
+<<boundary>> 
+- 클래스 이름: DeleteAccountUI
+- 클래스 설명: 사용자로부터 회원탈퇴 요청을 받은 후 DeleteAccount <<control>>에 멤버 정보를 전달한다.
+*/
+
 class DeleteAccountUI {
 	DeleteAccount* DeleteAccount_control;
 public:
 	DeleteAccountUI(DeleteAccount* control);
+	
+	//현재멤버 정보(curMem), memberDB에 저장된 member정보(memDB)를 DeleteAccount <<control>>에 전달한다.
 	void deleteMember(Member*& curMem, MemberDB& memDB);
 };
 #endif // !__DeleteAccount_H
