277967e8273182f39af993faaa0fc1a4e6043c17
xoxyunn <104340764+xoxyunn@users.noreply.github.com>
2023-05-25 22:26:25 +0900

    Update LogOut.h

--- 최종코드_주석X/LogOut.h
+++ 최종코드_주석X/LogOut.h
@@ -2,8 +2,15 @@
 #define __LogOut_H
 #include "Member.h"
 
+/*
+* <<control>>
+- 클래스 이름: LogOut
+- 클래스 설명: LogOutUI <<boundary>>로부터 호출되어 현재 Member를 로그아웃 처리한다.
+*/
+
 class LogOut {
 public:
+	//현재 멤버의 정보를 담고있는 curMem의 값을 NULL로 변경한다. 이후 이 값을 파일에 출력한다.
 	void requestLogOut(Member*& curMem);
 };
 
