a7c79c6f7421a6227cd6e0ab1ad040909880de35
OK-Bi <81093270+OK-Bi@users.noreply.github.com>
2023-05-25 22:34:14 +0900

    Update ShowMyEmployments.h

--- 최종코드_주석X/ShowMyEmployments.h
+++ 최종코드_주석X/ShowMyEmployments.h
@@ -1,3 +1,9 @@
+/*
+* <<control>>
+- 클래스 이름: ShowMyEmployments
+- 클래스 설명: ShowMyEmploymentsUI<<boundary>>에서 회사 회원 정보를 입력받고, 해당 회사 회원의 채용 정보 리스트(업무,인원수,마감일)을 출력한다.
+*/
+
 
 #ifndef ShowMyEmployments_h
 #define ShowMyEmployments_h
@@ -5,7 +11,7 @@
 #include "CorporateMember.h"
 
 // ShowMyEmployments 
-// ȸ�� ȸ���� ����� ä�� ����Ʈ�� ��� ���
+// 회사 회원이 등록한 채용 리스트를 모두 출력
 
 
 class ShowMyEmployments
