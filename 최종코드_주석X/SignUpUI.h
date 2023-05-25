12523869451f1e5759022502d0a2c2e36ed4081c
xoxyunn <104340764+xoxyunn@users.noreply.github.com>
2023-05-25 22:36:00 +0900

    Update SignUpUI.h

--- 최종코드_주석X/SignUpUI.h
+++ 최종코드_주석X/SignUpUI.h
@@ -4,6 +4,12 @@
 #include <string>
 #include "SignUp.h"
 
+/*
+<<boundary>>
+- 클래스 이름: SignUpUI
+- 클래스 설명: 사용자로부터 받은 정보를 SignUp <<control>>에 전달한다.
+*/
+
 class SignUpUI {
 	SignUp* SignUp_control;
 public:
