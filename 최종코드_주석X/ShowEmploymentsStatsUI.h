79bd97fcff6e9f86bfe19b0832c68eb41c5e33e0
OK-Bi <81093270+OK-Bi@users.noreply.github.com>
2023-05-26 01:40:13 +0900

    Update ShowEmploymentsStatsUI.h

--- 최종코드_주석X/ShowEmploymentsStatsUI.h
+++ 최종코드_주석X/ShowEmploymentsStatsUI.h
@@ -1,3 +1,9 @@
+/*
+* <<boundary>>
+- 클래스 이름: ShowEmploymentsStatsUI
+- 클래스 설명: 회사 회원으로부터 지원 정보 통계 요청이 들어오면 해당 회원 정보 curMem을 ShowEmploymentsStats<<control>>에 전달한다.
+*/
+
 #ifndef _ShowEmploymentsStatsUI_H
 #define _ShowEmploymentsStatsUI_H
 
