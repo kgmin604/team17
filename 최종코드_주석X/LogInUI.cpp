257eb7657e55da7fb8b5280133652d10cd8906ea
xoxyunn <104340764+xoxyunn@users.noreply.github.com>
2023-05-25 22:25:52 +0900

    Update LogInUI.cpp

--- 최종코드_주석X/LogInUI.cpp
+++ 최종코드_주석X/LogInUI.cpp
@@ -1,9 +1,16 @@
 #include "LogInUI.h"
 
+/*
+* <<boundary>>
+- 클래스 이름: LogInUI
+- 클래스 설명: 사용자로부터 로그인 요청이 들어오면 받은 정보값(id, pw 등)을 LogIn <<control>>에 전달한다.
+*/
+
 LogInUI::LogInUI(LogIn* control) {
 	LogIn_control = control;
 }
 
 void LogInUI::requestLogIn(Member*& curMem, string id, string password, MemberDB& memDB) {
+	//LogIn <<control>>에 현재 멤버, id, password, MemDB값을 전달한다.
 	LogIn_control->findMember(curMem, id, password, memDB);
 }
