e1846d71d5e9c9228a0a8b62ec64c66625ec87f1
OK-Bi <81093270+OK-Bi@users.noreply.github.com>
2023-05-25 22:28:43 +0900

    Update AddEmploymentUI.h

--- 최종코드_주석X/AddEmploymentUI.h
+++ 최종코드_주석X/AddEmploymentUI.h
@@ -1,3 +1,8 @@
+/*
+* <<boundary>>
+- 클래스 이름: AddEmploymentUI
+- 클래스 설명: 회사 회원에게서 받은 채용 정보(업무, 인원 수, 신청 마감일)를 AddEmployment<<control>>에 전달한다.
+*/
 
 #ifndef AddEmploymentUI_h
 #define AddEmploymentUI_h
