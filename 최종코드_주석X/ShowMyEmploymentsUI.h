2cade8009eb3046916a38312c55b399b6d73af30
OK-Bi <81093270+OK-Bi@users.noreply.github.com>
2023-05-25 22:31:21 +0900

    Update ShowMyEmploymentsUI.h

--- 최종코드_주석X/ShowMyEmploymentsUI.h
+++ 최종코드_주석X/ShowMyEmploymentsUI.h
@@ -1,3 +1,8 @@
+/*
+* <<boundary>>
+- 클래스 이름: ShowMyEmploymentsUI
+- 클래스 설명: 회사 회원으로부터 채용 정보 조회 요청이 들어오면 해당 회사 회원의 정보를 ShowMyEmployments<<control>>에 전달한다.
+*/
 
 #ifndef ShowMyEmploymentsUI_h
 #define ShowMyEmploymentsUI_h
@@ -5,7 +10,7 @@
 
 #include "ShowMyEmployments.h"
 
-// ShowMyEmployments control
+
 
 class ShowMyEmploymentsUI
 {
