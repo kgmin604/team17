ceaefd4c39bb49c21a55c3529044b58d028a7dd1
avatrue <mingi0831@naver.com>
2023-05-26 10:29:46 +0900

    Update ShowApplication.h

--- 최종코드_주석X/ShowApplication.h
+++ 최종코드_주석X/ShowApplication.h
@@ -9,11 +9,11 @@ using namespace std;
 /*
     <<control>>
 
-    - Ŭ���� �̸�: ShowApplication
-    - Ŭ���� ����:
-    ������� ������������Ʈ�� �޾ƿ´�.
-    �������� ����Ʈ�� �� ���������� ���� ȸ���̸� ����ڹ�ȣ ���� �ο� ��û ������ ������ �޾ƿ�
-    ���Ͽ� ����Ѵ�.
+    - 클래스 이름: ShowApplication
+    - 클래스 설명:
+    사용자의 지원정보리스트를 받아온다.
+    지원정보 리스트의 각 지원정보에 대해 회사이름 사업자번호 업무 인원 신청 마감일 정보를 받아와
+    파일에 출력한다.
 
 */
 
@@ -21,7 +21,7 @@ class ShowApplication
 {
 private:
 public:
-    void requestShowApplication(GeneralMember* curMem);
+    void requestShowApplication(GeneralMember* curMem);     //사용자의 지원정보 리스트를 받아와 회사이름 사업자번호 업무 인원 신청 마감일 정보를 출력한다.
 };
 
 #endif
