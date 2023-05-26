90470446a03ed8428f6274d812688ac38e83ba04
kgmin604 <70381385+kgmin604@users.noreply.github.com>
2023-05-26 11:18:02 +0900

    Add files via upload

---
+++ SignUp.cpp
@@ -0,0 +1,19 @@
+﻿#include "SignUp.h"
+#include <fstream>
+using namespace std;
+extern ofstream fout;
+
+void SignUp::addNewMember(int type, string id, string password, string name, string number, MemberDB& mem_db) {
+	if (type == 1) {
+		CorporateMember* new_mem = new CorporateMember(type, id, password, name, number);
+		mem_db.addMember(new_mem);
+	}
+	else if (type == 2) {
+		GeneralMember* new_mem = new GeneralMember(type, id, password, name, number);
+		mem_db.addMember(new_mem);
+	}
+	
+	fout << "1.1. 회원가입\n";
+	fout << "> " << type << " " << name << " " << number << " " << id << " " << password << "\n";
+	fout << "\n";
+}
