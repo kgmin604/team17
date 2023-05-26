9915910aa91fd6a2835f39dc37c15ada95cd672f
kgmin604 <70381385+kgmin604@users.noreply.github.com>
2023-05-26 09:01:50 +0900

    Update showMyStatsUI.h

--- 최종코드_주석X/showMyStatsUI.h
+++ 최종코드_주석X/showMyStatsUI.h
@@ -3,6 +3,13 @@
 
 #include "showMyStats.h"
 
+/*
+    <<boundary>>
+    - 클래스 이름: showMyStatsUI
+    - 클래스 설명:
+    사용자에게 지원 정보 통계 요청을 받은후 요청을 control클래스에 넘겨준다.
+*/
+
 class showMyStatsUI {
 	showMyStats* showMyStats_control;
 public:
